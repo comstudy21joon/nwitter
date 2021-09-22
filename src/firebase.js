3882644b5907b905d329152f4ffa1d74ab8e7133
comstudy21 <comstudy21@naver.com>
2021-09-22 23:27:52 +0900

    2.0 react firebase 설정 완료

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+//firebase v9..방식에서는 firebase/compat/... 사용
+//import firebase from "firebase/compat/app";
+//firebase v7.20.0 방식
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// firebase 모듈 import 후 확인
+import firebase from "./firebase";
+console.log(firebase);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
